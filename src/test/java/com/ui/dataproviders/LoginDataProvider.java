991291c7bddefd381c9fabe6770fbb28bd87de04
BettyRavi10 <betty.sn@gmail.com>
2026-08-30 19:26:17 -0400

    Updated login data provider

--- src/test/java/com/ui/dataproviders/LoginDataProvider.java
+++ src/test/java/com/ui/dataproviders/LoginDataProvider.java
@@ -20,7 +20,7 @@ public class LoginDataProvider {
 	@DataProvider(name = "loginDataProvider")
 	public Iterator<Object[]> loginDataProvider() throws FileNotFoundException {
 		Gson gson = new Gson();
-		File testDataFile = new File(System.getProperty("user.dir") + "\\testData\\logindata.json");
+		File testDataFile = new File(System.getProperty("user.dir") + "//testData//logindata.json");
 		FileReader fileReader = new FileReader(testDataFile);
 		TestData testdata = gson.fromJson(fileReader, TestData.class); // Deserialization of JSON to Java object.
 																		// Here we are converting JSON to TestData class
